e348fa014c33b769af20ba3bb34bd3774710b04f
kasun789 <kasungayashan789@gmail.com>
2023-09-27 11:34:14 +0530

    add CircleShape.java

---
+++ CircleShape.java
@@ -0,0 +1,10 @@
+import Shapeactivity;
+
+package test04;
+public class CircleShape implements Shapeactivity {
+    @Override
+    public void draw() {
+        System.out.println("Inside CircleShape:: draw() method.");
+    }
+
+}
